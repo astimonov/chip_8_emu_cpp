b63d76510c801399709f2e46d4c82cd3b8cf2705
Alexander Timonov <astimonov@gmail.com>
2019-07-21 21:36:33 +0300

    Added set pc function

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -5,3 +5,7 @@ using namespace emulator;
 Cpu::Cpu(const std::shared_ptr<Ram>& ram) : m_ram(ram) {
 
 }
+
+void Cpu::SetRegPC(uint16_t pc) {
+    this->m_reg_pc = pc;
+}
--- src/cpu.hpp
+++ src/cpu.hpp
@@ -11,13 +11,13 @@ namespace emulator {
     class Cpu {
     public:
         explicit Cpu(const std::shared_ptr<Ram>& ram);
+        void SetRegPC(uint16_t pc);
     private:
         static constexpr int STACK_DEPTH = 16;
         static constexpr int GP_REGISTERS_QTY = 16;
         std::array<uint16_t, STACK_DEPTH> m_stack;
         std::array<uint8_t, GP_REGISTERS_QTY> m_reg_v;
         uint16_t m_reg_i;
-        uint16_t m_reg_vf;
         uint16_t m_reg_pc;
         uint16_t m_reg_sp;
         const std::shared_ptr<Ram>& m_ram;
