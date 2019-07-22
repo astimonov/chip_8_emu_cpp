c9b1408d4756cf0e651883a278d178e2bc06a754
Alexander Timonov <astimonov@gmail.com>
2019-07-22 21:41:02 +0300

    Added CPU reset function

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -9,3 +9,11 @@ Cpu::Cpu(const std::shared_ptr<Ram>& ram) : m_ram(ram) {
 void Cpu::SetRegPC(uint16_t pc) {
     this->m_reg_pc = pc;
 }
+
+void Cpu::Reset() {
+    std::fill(this->m_reg_v.begin(), this->m_reg_v.end(), GP_REGISTER_RESET_VALUE);
+    std::fill(this->m_stack.begin(), this->m_stack.end(), STACK_RESET_VALUE);
+    this->m_reg_i = AUX_REGISTER_RESET_VALUE;
+    this->m_reg_pc = AUX_REGISTER_RESET_VALUE;
+    this->m_reg_sp = AUX_REGISTER_RESET_VALUE;
+}
--- src/cpu.hpp
+++ src/cpu.hpp
@@ -12,9 +12,13 @@ namespace emulator {
     public:
         explicit Cpu(const std::shared_ptr<Ram>& ram);
         void SetRegPC(uint16_t pc);
+        void Reset();
     private:
         static constexpr int STACK_DEPTH = 16;
+        static constexpr uint16_t STACK_RESET_VALUE = 0x0000;
         static constexpr int GP_REGISTERS_QTY = 16;
+        static constexpr uint8_t GP_REGISTER_RESET_VALUE = 0x00;
+        static constexpr uint8_t AUX_REGISTER_RESET_VALUE = 0x00;
         std::array<uint16_t, STACK_DEPTH> m_stack;
         std::array<uint8_t, GP_REGISTERS_QTY> m_reg_v;
         uint16_t m_reg_i;
