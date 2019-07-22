b1d60cc2e711436cd9651c68e653df7ef6443019
Alexander Timonov <astimonov@gmail.com>
2019-07-22 21:41:16 +0300

    Added ROM read invocation

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -6,6 +6,7 @@ using namespace emulator;
 Emulator::Emulator(Rom rom) : m_rom(rom) {
     this->m_ram = std::make_shared<Ram>();
     this->m_cpu = std::make_unique<Cpu>(this->m_ram);
-    // TODO: Move ROM memory to RAM
+    this->m_ram->WriteMultiple(this->m_rom.GetData(), Emulator::PC_START);
+    this->m_cpu->Reset();
     this->m_cpu->SetRegPC(PC_START);
 }
