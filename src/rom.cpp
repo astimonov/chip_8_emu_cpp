f19b9a672192446d97ffeda57a198d6e03e55155
Alexander Timonov <astimonov@gmail.com>
2019-07-22 21:40:17 +0300

    Implemented Rom constructor

--- src/rom.cpp
+++ src/rom.cpp
@@ -1,9 +1,23 @@
+#include <fstream>
+
 #include "rom.hpp"
 
 using namespace emulator;
 
 Rom::Rom(std::string filename) {
-    // TODO: Read ROM from file or throw an exception
+    std::ifstream file(filename, std::ios::binary);
+    file.unsetf(std::ios::skipws);
+
+    file.seekg(0, std::ios::end);
+    std::vector<uint8_t> rom_data;
+    rom_data.reserve(file.tellg());
+    file.seekg(0, std::ios::beg);
+
+    rom_data.insert(rom_data.begin(),
+            std::istreambuf_iterator<char>(file),
+            std::istreambuf_iterator<char>());
+
+    this->m_data = rom_data;
 }
 
 std::vector<uint8_t> Rom::GetData() {
