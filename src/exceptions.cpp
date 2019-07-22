5de71b56ae6105335b4e437ac7039951b06327cb
Alexander Timonov <astimonov@gmail.com>
2019-07-22 23:16:25 +0300

    Added RuntimeException class

---
+++ src/exceptions.cpp
@@ -0,0 +1,19 @@
+#include "exceptions.hpp"
+
+emulator::RuntimeException::RuntimeException(uint16_t pc, uint16_t opcode) :
+    m_pc(pc), m_opcode(opcode)
+{
+    this->m_message = "Emulator runtime error";
+}
+
+const char *emulator::RuntimeException::what() const noexcept {
+    return this->m_message;
+}
+
+uint16_t emulator::RuntimeException::GetPc() {
+    return this->m_pc;
+}
+
+uint16_t emulator::RuntimeException::GetOpcode() {
+    return this->m_opcode;
+}
---
+++ src/exceptions.hpp
@@ -0,0 +1,22 @@
+#ifndef CHIP_8_EMU_CPP_EXCEPTIONS_HPP
+#define CHIP_8_EMU_CPP_EXCEPTIONS_HPP
+
+#include <exception>
+#include <cstdint>
+
+namespace emulator {
+    class RuntimeException : public std::exception {
+    public:
+        RuntimeException(uint16_t pc, uint16_t opcode);
+        const char* what() const noexcept;
+        uint16_t GetPc();
+        uint16_t GetOpcode();
+    private:
+        const char * m_message;
+        uint16_t m_pc;
+        uint16_t m_opcode;
+    };
+}
+
+
+#endif //CHIP_8_EMU_CPP_EXCEPTIONS_HPP
