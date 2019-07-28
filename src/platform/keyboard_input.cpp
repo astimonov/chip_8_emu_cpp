3f7069e7dbe2f9632a0c0b547848d6d6073ce8c5
Alexander Timonov <astimonov@gmail.com>
2019-07-28 20:03:19 +0300

    Added input handling

--- src/platform/keyboard_input.cpp
+++ src/platform/keyboard_input.cpp
@@ -1,6 +1,7 @@
-//
-// Created by Alexander on 25.07.2019.
-//
+#include <exception>
+#include <map>
+
+#include <SDL.h>
 
 #include "keyboard_input.hpp"
 
@@ -15,5 +16,32 @@ bool KeyboardInput::IsKeyPressed(uint32_t key) {
 }
 
 void KeyboardInput::Scan() {
+    const Uint8 *state = SDL_GetKeyboardState(NULL);
 
+    if (state == nullptr) {
+        throw std::runtime_error("Could not get keyboard state");
+    }
+
+    static std::map<uint32_t, SDL_Scancode> mapping = {
+            { 0x0, SDL_SCANCODE_1 },
+            { 0x1, SDL_SCANCODE_2 },
+            { 0x2, SDL_SCANCODE_3 },
+            { 0x3, SDL_SCANCODE_4 },
+            { 0x4, SDL_SCANCODE_Q },
+            { 0x5, SDL_SCANCODE_W },
+            { 0x6, SDL_SCANCODE_E },
+            { 0x7, SDL_SCANCODE_R },
+            { 0x8, SDL_SCANCODE_A },
+            { 0x9, SDL_SCANCODE_S },
+            { 0xA, SDL_SCANCODE_D },
+            { 0xB, SDL_SCANCODE_F },
+            { 0xC, SDL_SCANCODE_Z },
+            { 0xD, SDL_SCANCODE_X },
+            { 0xE, SDL_SCANCODE_C },
+            { 0xF, SDL_SCANCODE_V },
+    };
+
+    for (auto const& [key, scancode] : mapping) {
+        this->m_keys[key] = state[scancode];
+    }
 }
