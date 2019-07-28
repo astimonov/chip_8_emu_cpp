5f7d325794f3ca7cd1dc88b1976009d44c5b04ee
Alexander Timonov <astimonov@gmail.com>
2019-07-28 20:03:28 +0300

    Added graphics handling

--- src/platform/sdl2_graphics.cpp
+++ src/platform/sdl2_graphics.cpp
@@ -1,6 +1,6 @@
-//
-// Created by Alexander on 25.07.2019.
-//
+#include <exception>
+
+#include <SDL.h>
 
 #include "sdl2_graphics.hpp"
 
@@ -8,20 +8,65 @@ using namespace emulator;
 
 SDL2Graphics::SDL2Graphics() {
     this->Reset();
+
+    this->m_window = SDL_CreateWindow("CHIP-8 Emulator",
+                                      SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 320,
+                                      SDL_WINDOW_RESIZABLE);
+    if (this->m_window == nullptr) {
+        throw std::runtime_error("Could not create window");
+    }
+
+    this->m_renderer = SDL_CreateRenderer(this->m_window, -1, SDL_RENDERER_ACCELERATED);
+
+    if (this->m_renderer == nullptr) {
+        SDL_DestroyWindow(this->m_window);
+        throw std::runtime_error("Could not create renderer");
+    }
+
+    this->m_texture = SDL_CreateTexture(this->m_renderer, SDL_PIXELFORMAT_ARGB8888,
+                                        SDL_TEXTUREACCESS_STREAMING,
+                                        IGraphics::GFX_MAX_X, IGraphics::GFX_MAX_Y);
+
+    if (this->m_texture == nullptr) {
+        SDL_DestroyRenderer(this->m_renderer);
+        SDL_DestroyWindow(this->m_window);
+        throw std::runtime_error("Could not create texture");
+    }
+
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, 0);
+}
+
+SDL2Graphics::~SDL2Graphics() {
+    SDL_DestroyRenderer(this->m_renderer);
+    SDL_DestroyWindow(this->m_window);
 }
 
 void SDL2Graphics::Reset() {
-    this->m_graphics_memory = {};
+    for (auto& y : this->m_graphics_memory) {
+        for (auto& x : y) {
+            x = PALETTE_PIXEL_0;
+        }
+    }
 }
 
 void SDL2Graphics::Draw() {
 
+    SDL_Rect rect;
+    rect.x = 0;
+    rect.y = 0;
+    SDL_GetWindowSize(this->m_window, &rect.w, &rect.h);
+
+    SDL_UpdateTexture(this->m_texture, nullptr, this->m_graphics_memory.data(),
+                IGraphics::GFX_MAX_X * sizeof(uint32_t));
+    SDL_RenderClear(this->m_renderer);
+    SDL_RenderCopy(this->m_renderer, this->m_texture, nullptr, &rect);
+    SDL_RenderPresent(this->m_renderer);
 }
 
 uint8_t SDL2Graphics::GetXY(uint32_t x, uint32_t y) {
-    return 0;
+    return SDL2Graphics::NormalizeColor(this->m_graphics_memory[y][x]);
 }
 
 void SDL2Graphics::SetXY(uint32_t x, uint32_t y, uint8_t value) {
-
+    this->m_graphics_memory[y][x] = SDL2Graphics::DenormalizeColor(value);
 }
--- src/platform/sdl2_graphics.hpp
+++ src/platform/sdl2_graphics.hpp
@@ -9,12 +9,27 @@ namespace emulator {
     class SDL2Graphics : public IGraphics {
     public:
         SDL2Graphics();
+        ~SDL2Graphics();
         void Reset();
         void Draw();
         uint8_t GetXY(uint32_t x, uint32_t y);
         void SetXY(uint32_t x, uint32_t y, uint8_t value);
     private:
-        std::array<std::array<uint8_t, IGraphics::GFX_MAX_Y>, IGraphics::GFX_MAX_X> m_graphics_memory;
+        static constexpr uint32_t PALETTE_PIXEL_1 = 0x000080FF;
+        static constexpr uint32_t PALETTE_PIXEL_0 = 0x00003366;
+
+        std::array<std::array<uint32_t, IGraphics::GFX_MAX_X>, IGraphics::GFX_MAX_Y> m_graphics_memory;
+        SDL_Window *m_window;
+        SDL_Renderer *m_renderer;
+        SDL_Texture *m_texture;
+
+        static constexpr uint32_t DenormalizeColor(uint8_t normalized) {
+            return normalized ? SDL2Graphics::PALETTE_PIXEL_1 : SDL2Graphics::PALETTE_PIXEL_0;
+        }
+
+        static constexpr uint8_t NormalizeColor(uint32_t color) {
+            return color == SDL2Graphics::PALETTE_PIXEL_1 ? 1 : 0;
+        }
     };
 }
 
